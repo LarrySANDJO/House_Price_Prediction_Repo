ef1fdff2079f9521045933abea8d23068708b742
LarrySANDJO <larrysandjo1@gmail.com>
2024-12-15 09:00:30 +0000

    Ajout du dossier Data dans st..app

--- Streamlit_App/my_pages/accueil.py
+++ Streamlit_App/my_pages/accueil.py
@@ -6,11 +6,8 @@ import plotly.express as px
 def display():
 
 #-------------------------------------------- Chargement des donnes
-    df = pd.read_csv("Data/Raw/train.csv")
+    df = pd.read_csv("HOUSE_PRICE_PREDICTION/Data/Raw/train.csv")
         
-
-
-
 #-------------------------------------------- Chargement du css pour les textes
     st.markdown("""
     <style>
@@ -49,99 +46,58 @@ def display():
     """, unsafe_allow_html=True)
 
 #-------------------------------------------- Affichage des donnes clef:Total produit,...
-
-    total_products = st.session_state["df"]["product_id"].nunique()
-    total_categories = st.session_state["df"]["category_id"].nunique()
-    total_subcategories = st.session_state["df"]["subcategory_id"].nunique()
-    out_of_stock_products = st.session_state["df"][st.session_state["df"]["is_out_of_stock"] == True].shape[0]
-    on_promotion = st.session_state["df"][st.session_state["df"]["old_price"] != "Not concerned"].shape[0]
+    mean_price = df["SalePrice"].mean()
+    median_price = df["SalePrice"].median()
+    price_variance = df["SalePrice"].var()
+    max_price = df["SalePrice"].max()
+    min_price = df["SalePrice"].min()
 
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.info('Total de produits',icon="📌")
-        display_custom_metric("Total de produits", total_products, "#0000FF")
+        st.info('Moyenne des prix',icon="📌")
+        display_custom_metric("Moyenne des prix", int(mean_price), "#0000FF")
     with col2:
-        st.info('Catégories',icon="📌")
-        display_custom_metric("Catégories", total_categories, "#228B22")
+        st.info('Médiane des prix',icon="📌")
+        display_custom_metric("Médiane des prix", int(median_price), "#228B22")
     with col3:
-        st.info('Sous-catégories',icon="📌")
-        display_custom_metric("Sous-catégories", total_subcategories, "#FF0000")
+        st.info('Variance des prix',icon="📌")
+        display_custom_metric("Variance des prix", int(price_variance), "#FF0000")
     with col4:
-        st.info('En rupture de stock',icon="⚠️")
-        display_custom_metric("En rupture de stock", out_of_stock_products, "#FE9900")
+        st.info('Prix maximal',icon="📌")
+        display_custom_metric("Prix maximal", int(max_price), "#FE9900")
     with col5:
-        st.info('En promotion',icon="💯")
-        display_custom_metric("En promotion", on_promotion, "#582698")
+        st.info('Prix minimal',icon="📌")
+        display_custom_metric("Prix minimal", int(min_price), "#582698")
 
 
     st.markdown("---")
+    
+    
+#----------------------------------------------- Graphique croise avec les 10 variables qui infuencent le plus 
 
-#--------------------------------------------Affichage du produit le plus cher et le moins 
-    max_price_product = st.session_state["df"].loc[st.session_state["df"]['price'].idxmax()]
-    min_price_product = st.session_state["df"].loc[st.session_state["df"]['price'].idxmin()]
-    col1, col2 = st.columns([2, 2])
-
-#-------------------------------------------- redefinition perso de display_product_info()
-    def display_product_info(product):
-        product_name = product.get("title", "Nom non disponible")
-        product_price = product.get("price", "Prix non disponible")
-        product_image = product.get("image_url", "")
-        product_status = "En rupture de stock" if product.get("is_out_of_stock") else "En stock"
-        st.markdown(
-            """
-            <style>
-            .product-card {
-                border: 1px solid #d1d1d1;
-                border-radius: 8px;
-                padding: 16px;
-                margin-bottom: 16px;
-                background-color: #f9f9f9;
-                text-align: center;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-        st.markdown(f"""
-        <div class="product-card">
-            <img src="{product_image}" alt="Image du produit" style="width:50%; height:auto; border-radius: 8px;">
-            <h4>{product_name}</h4>
-            <p style = "font-weight: bold;font-size: 24px;">Prix : {product_price} CFA</p>
-            <p>Statut : {product_status}</p>
-        </div>
-        """, unsafe_allow_html=True)
-
-    #
-
-    with col1: 
+    Best_quanti = ["GarageCars","YearBuilt","FullBath","GarageArea"]
+    
+    Best_quali = ["OverallQual","Neighborhood","ExterQual","BsmtQual","KitchenQual","Alley","GarageFinish"]
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
         st.markdown("""
             <div class="dashboard-header animate-fade-in">
-                <h3 style = "text-align: center;font-weight: bold;">Produit le plus cher</h3>
+                <h3 style = "text-align: center;font-weight: bold;">Graphique croise avec les variables quantitatives</h3>
             </div>
         """, unsafe_allow_html=True)
-        display_product_info(max_price_product)
+        
+        st.selectbox("Selectionnez une variable", options = Best_quanti, key = "key_quanti")
+        
+        if st.session_state['key_quanti']:
+            fig = px.scatter(df, x = "{st.session_state['key_quanti']}", y='SalePrice', title=f"Graphique de {st.session_state['key_quanti']} vs SalePrice",
+                            labels={st.session_state['key_quanti']: st.session_state['key_quanti'], 'SalePrice': 'SalePrice'})
 
-    with col2: 
-        st.markdown("""
-            <div class="dashboard-header animate-fade-in">
-                <h3 style = "text-align: center; font-weight: bold;">Produit le moins cher</h3>
-            </div>
-        """, unsafe_allow_html=True)
-        display_product_info(min_price_product)
-
-    st.markdown("---")
-#--------------------------------------------Graphique de repartition par categorie
-
-    col1, col2 = st.columns([2, 2])
-
-    category_count = st.session_state["df"]["category"].value_counts()
-    fig = px.pie(
-        values=category_count,
-        names=category_count.index,
-        title="Répartition par catégorie",
-    )
-    fig.update_layout(title_font_size=24, legend_font_size=16)
-    with col1: st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig)
+    
+    
+    
 
 #--------------------------------------------Affichage des produits en rupture de stock par categorie
 
